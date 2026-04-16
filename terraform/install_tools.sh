0e135047113c63b754fd119fc3659860395e8f55
kiranxops <kiranshevale2020@gmail.com>
2026-04-16 17:01:26 +0530

    update jenkin version

--- terraform/install_tools.sh
+++ terraform/install_tools.sh
@@ -6,9 +6,9 @@ sudo apt install fontconfig openjdk-21-jre
 
 
 # Jenkins installation
-sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
-echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
